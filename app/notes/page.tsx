5025ead738f166a6d6fc0616e97429e0500f549c
kihyeoon <kihyeoon@gmail.com>
2025-01-25 15:32:02 +0900

    feat: select notes database

---
+++ app/notes/page.tsx
@@ -0,0 +1,32 @@
+// "use client";
+
+// import { createClient } from "@/utils/supabase/client";
+// import { useEffect, useState } from "react";
+
+// export default function Page() {
+//   const [notes, setNotes] = useState<any[] | null>(null);
+//   const supabase = createClient();
+
+//   useEffect(() => {
+//     const getData = async () => {
+//       const { data } = await supabase.from("notes").select();
+//       setNotes(data);
+//     };
+//     getData();
+//   }, []);
+
+//   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+// }
+
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: notes, error } = await supabase.from("notes").select();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+}
